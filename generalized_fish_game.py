e237595b255f98f76a711c417343e7609f2886e1
Antonia Hadjimichael <ah986@cornell.edu>
2019-06-27 14:14:51 -0400

    back to old noise

--- generalized_fish_game.py
+++ generalized_fish_game.py
@@ -55,10 +55,10 @@ def fish_game(vars, # contains all C, R, W
     cnstr = [0.0]*nCnstr
     
     # Create array with environmental stochasticity for prey
-    epsilon_prey = np.random.normal(0.0, np.sqrt(sigmaX), N)
+    epsilon_prey = np.random.normal(0.0, sigmaX, N)
     
     # Create array with environmental stochasticity for predator
-    epsilon_predator = np.random.normal(0.0, np.sqrt(sigmaY), N)
+    epsilon_predator = np.random.normal(0.0, sigmaY, N)
 
     # Go through N possible realizations
     for i in range(N):
