be4740aa7cddcd5d8ea4da865f46c9c8037c756f
Antonia Hadjimichael <ah986@cornell.edu>
2019-06-27 14:33:30 -0400

    updated epsilons

--- find_refSets.sh
+++ find_refSets.sh
@@ -3,5 +3,5 @@
 source /etc/profile.d/modules.sh
 module load python-2.7.5
 
-python pareto.py ./Generalized/sets/*.set -o 6-9 -e 5 0.005 1 1   --output Generalized.resultfile --delimiter=" " --comment="#"
+python pareto.py ./Generalized/sets/*.set -o 6-9 -e 5 0.005 1 1 5   --output Generalized.resultfile --delimiter=" " --comment="#"
 cut -d ' ' -f 7-10 Generalized.resultfile >Generalized.reference
