5dd6bcd0efc3b14ec00e4829acc4be8622ffd3bc
antonia-had <ah986@cornell.edu>
2019-06-27 20:39:26 -0400

    fixed constraint nan

--- parallel_coordinate_multiple_SOW.py
+++ parallel_coordinate_multiple_SOW.py
@@ -10,10 +10,11 @@ objs_labels = ['Net present\nvalue (NPV)',
                'Prey population deficit', 
                'Longest duration\nof low harvest', 
                'Worst harvest instance',
+               'Variance of harvest',
                'Duration of predator\npopulation collapse']
 
 nSamples=4000 # Number of SOW
-nObjs=4
+nObjs=5
 nCnstr = 1
 numPts = (len(os.listdir('./Generalized/resim_objs'))) # Get number of solutions on pareto front
 
@@ -58,38 +59,47 @@ norm_alternative3 = optimized_alternative3.copy()
 norm_objs = objs.copy()
 
 # Normalize pairs of sets for each SOW
-mins1 = np.zeros([2,4])
-maxs1 = np.zeros([2,4])
+mins1 = np.zeros([2,nObjs])
+maxs1 = np.zeros([2,nObjs])
 mins1[0] = optimized_alternative1.min(axis=0)
 maxs1[0] = optimized_alternative1.max(axis=0)
 mins1[1] = objs[:,:,:].min(axis=1)[0]
 maxs1[1] = objs[:,:,:].max(axis=1)[0]
 
-mins2 = np.zeros([2,4])
-maxs2 = np.zeros([2,4])
+mins2 = np.zeros([2,nObjs])
+maxs2 = np.zeros([2,nObjs])
 mins2[0] = optimized_alternative2.min(axis=0)
 maxs2[0] = optimized_alternative2.max(axis=0)
 mins2[1] = objs[:,:,:].min(axis=1)[1]
 maxs2[1] = objs[:,:,:].max(axis=1)[1]
 
-mins3 = np.zeros([2,4])
-maxs3 = np.zeros([2,4])
+mins3 = np.zeros([2,nObjs])
+maxs3 = np.zeros([2,nObjs])
 mins3[0] = optimized_alternative3.min(axis=0)
 maxs3[0] = optimized_alternative3.max(axis=0)
 mins3[1] = objs[:,:,:].min(axis=1)[2]
 maxs3[1] = objs[:,:,:].max(axis=1)[2]
 
-for i in range(4):
+for i in range(nObjs):
     norm_alternative1[:,i] = (optimized_alternative1[:,i] - np.min(mins1[:,i])) / (np.max(maxs1[:,i]) - np.min(mins1[:,i]))
     norm_alternative2[:,i] = (optimized_alternative2[:,i] - np.min(mins2[:,i])) / (np.max(maxs2[:,i]) - np.min(mins2[:,i]))
     norm_alternative3[:,i] = (optimized_alternative3[:,i] - np.min(mins3[:,i])) / (np.max(maxs3[:,i]) - np.min(mins3[:,i]))
     norm_objs[0,:,i] = (objs[0,:,i] - np.min(mins1[:,i])) / (np.max(maxs1[:,i]) - np.min(mins1[:,i]))
     norm_objs[1,:,i] = (objs[1,:,i] - np.min(mins2[:,i])) / (np.max(maxs2[:,i]) - np.min(mins2[:,i]))
     norm_objs[2,:,i] = (objs[2,:,i] - np.min(mins3[:,i])) / (np.max(maxs3[:,i]) - np.min(mins3[:,i]))
-    
-norm_cnstr1 = (cnstr[0]-np.min(cnstr[0]))/(np.max(cnstr[0])-np.min(cnstr[0]))
-norm_cnstr2 = (cnstr[1]-np.min(cnstr[1]))/(np.max(cnstr[1])-np.min(cnstr[1]))
-norm_cnstr3 = (cnstr[2]-np.min(cnstr[2]))/(np.max(cnstr[2])-np.min(cnstr[2]))
+
+if np.max(cnstr[0])==np.min(cnstr[0]):
+    norm_cnstr1 = np.zeros(len(cnstr[0,:]))
+else:
+    norm_cnstr1 = (cnstr[0]-np.min(cnstr[0]))/(np.max(cnstr[0])-np.min(cnstr[0]))
+if np.max(cnstr[1])==np.min(cnstr[1]):   
+    norm_cnstr2 = np.zeros(len(cnstr[0,:]))
+else:
+    norm_cnstr2 = (cnstr[1]-np.min(cnstr[1]))/(np.max(cnstr[1])-np.min(cnstr[1]))
+if np.max(cnstr[2])==np.min(cnstr[2]):
+    norm_cnstr3 = np.zeros(len(cnstr[0,:]))
+else:
+    norm_cnstr3 = (cnstr[2]-np.min(cnstr[2]))/(np.max(cnstr[2])-np.min(cnstr[2]))
 
 # Define colormap for each SOW
 cmap1 = matplotlib.cm.get_cmap("Oranges")
@@ -158,11 +168,13 @@ minvalues = ["{0:.2f}".format(np.min(mins1, axis=0)[0]),
              "{0:.2f}".format(-np.min(mins1, axis=0)[1]), 
              str(-np.min(mins1, axis=0)[2]), 
              "{0:.2f}".format(np.min(mins1, axis=0)[3]), 
+             "{0:.2f}".format(-np.min(mins1, axis=0)[4]), 
              str(-np.min(cnstr[0]))]
 maxvalues = ["{0:.2f}".format(np.max(maxs1, axis=0)[0]), 
              "{0:.2f}".format(-np.max(maxs1, axis=0)[1]), 
              str(-np.max(maxs1, axis=0)[2]), 
              "{0:.2f}".format(np.max(maxs1, axis=0)[3]), 
+             "{0:.2f}".format(-np.max(maxs1, axis=0)[4]),
              str(-np.max(cnstr[0]))]
 #make a twin axis for toplabels
 ax1t = ax1.twiny()
@@ -175,11 +187,13 @@ minvalues = ["{0:.2f}".format(np.min(mins2, axis=0)[0]),
              "{0:.2f}".format(-np.min(mins2, axis=0)[1]), 
              str(-np.min(mins2, axis=0)[2]), 
              "{0:.2f}".format(np.min(mins2, axis=0)[3]), 
+             "{0:.2f}".format(-np.min(mins2, axis=0)[4]), 
              str(-np.min(cnstr[1]))]
 maxvalues = ["{0:.2f}".format(np.max(maxs2, axis=0)[0]), 
              "{0:.2f}".format(-np.max(maxs2, axis=0)[1]), 
              str(-np.max(maxs2, axis=0)[2]), 
-             "{0:.2f}".format(np.max(maxs2, axis=0)[3]), 
+             "{0:.2f}".format(np.max(maxs2, axis=0)[3]),
+             "{0:.2f}".format(-np.max(maxs2, axis=0)[4]),
              str(-np.max(cnstr[1]))]
 #make a twin axis for toplabels
 ax2t = ax2.twiny()
@@ -191,12 +205,14 @@ ax2t.set_xticklabels(maxvalues)
 minvalues = ["{0:.2f}".format(np.min(mins3, axis=0)[0]), 
              "{0:.2f}".format(-np.min(mins3, axis=0)[1]), 
              str(-np.min(mins3, axis=0)[2]), 
-             "{0:.2f}".format(np.min(mins3, axis=0)[3]), 
+             "{0:.2f}".format(np.min(mins3, axis=0)[3]),
+             "{0:.2f}".format(-np.min(mins3, axis=0)[4]), 
              str(-np.min(cnstr[2]))]
 maxvalues = ["{0:.2f}".format(np.max(maxs3, axis=0)[0]), 
              "{0:.2f}".format(-np.max(maxs3, axis=0)[1]), 
              str(-np.max(maxs3, axis=0)[2]), 
              "{0:.2f}".format(np.max(maxs3, axis=0)[3]), 
+             "{0:.2f}".format(-np.max(maxs3, axis=0)[4]), 
              str(-np.max(cnstr[2]))]
 #make a twin axis for toplabels
 ax3t = ax3.twiny()
