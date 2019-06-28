ed7158fce526687792e39663439adb3bc8061196
antonia-had <ah986@cornell.edu>
2019-06-27 21:01:46 -0400

    robustness

--- calcRobustness.py
+++ calcRobustness.py
@@ -49,11 +49,11 @@ def calcSatisfaction(objs,cnstr):
                 SatisfyMatrix[i,j,2] = 1
             if objs[i,j,3] <= -50:
                 SatisfyMatrix[i,j,3] = 1
-            if objs[i,j,4] <= 20000:
-                SatisfyMatrix[i,j,3] = 1
+            if objs[i,j,4] <= 30000:
+                SatisfyMatrix[i,j,4] = 1
             if cnstr[i,j] <= 0:
                 SatisfyMatrix[i,j,5] = 1                 
-            if objs[i,j,0] <= -1500 and objs[i,j,1] <= 0.2 and objs[i,j,2] <= 5 and objs[i,j,3] <= -50 and objs[i,j,4] <= 20000 and cnstr[i,j] <= 0:
+            if objs[i,j,0] <= -1500 and objs[i,j,1] <= 0.2 and objs[i,j,2] <= 5 and objs[i,j,3] <= -50 and objs[i,j,4] <= 30000 and cnstr[i,j] <= 0:
                 SatisfyMatrix[i,j,6] = 1
                 
     satisfaction = np.zeros([np.shape(SatisfyMatrix)[0],np.shape(SatisfyMatrix)[2]])
--- robustness_parallel_coordinate.py
+++ robustness_parallel_coordinate.py
@@ -13,33 +13,34 @@ objs_labels = ['Net present\nvalue (NPV) > 1500',
                'Prey population\ndeficit < 0.2', 
                'Longest duration\nof low harvest < 5', 
                'Worst harvest\ninstance > 50',
+               'Variance of harvest < 30000',
                'Duration of predator\npopulation collapse < 1']
 
 
 cmap = matplotlib.cm.get_cmap("Purples")
-robustness = robustness[robustness[:,5].argsort()]
-normalized_color = robustness[:,5]/np.max(robustness[:,5])-np.min(robustness[:,5])
+robustness = robustness[robustness[:,-1].argsort()]
+normalized_color = robustness[:,-1]/np.max(robustness[:,-1])-np.min(robustness[:,-1])
 for i in range(len(robustness[:,0])):
     ys = robustness[i,:]
     xs = range(len(ys))
-    ax.plot(xs[0:5], ys[0:5], c=cmap(normalized_color[i]), linewidth=2) 
+    ax.plot(xs[0:6], ys[0:6], c=cmap(normalized_color[i]), linewidth=2) 
     
 # Highlight specific solutions
 ys = robustness[np.argmax(robustness[:,0]),:] # Most robust in NPV
 xs = range(len(ys))
-l1=ax.plot(xs[0:5], ys[0:5], c=cmap(normalized_color[np.argmax(robustness[:,0])]), linewidth=3, label='Most robust in NPV', path_effects=[pe.Stroke(linewidth=6, foreground='darkgoldenrod'), pe.Normal()])
+l1=ax.plot(xs[0:6], ys[0:6], c=cmap(normalized_color[np.argmax(robustness[:,0])]), linewidth=3, label='Most robust in NPV', path_effects=[pe.Stroke(linewidth=6, foreground='darkgoldenrod'), pe.Normal()])
 
-ys = robustness[np.argmax(robustness[:,5]),:] # Most robust in all criteria
+ys = robustness[np.argmax(robustness[:,-1]),:] # Most robust in all criteria
 xs = range(len(ys))
-l2=ax.plot(xs[0:5], ys[0:5], c=cmap(normalized_color[np.argmax(robustness[:,5])]), linewidth=3, label='Most robust across criteria', path_effects=[pe.Stroke(linewidth=6, foreground='gold'), pe.Normal()])
+l2=ax.plot(xs[0:6], ys[0:6], c=cmap(normalized_color[np.argmax(robustness[:,5])]), linewidth=3, label='Most robust across criteria', path_effects=[pe.Stroke(linewidth=6, foreground='gold'), pe.Normal()])
 
 sm = matplotlib.cm.ScalarMappable(cmap=cmap)
-sm.set_array([robustness[:,5].min(),robustness[:,5].max()])
+sm.set_array([robustness[:,-1].min(),robustness[:,-1].max()])
 cbar = fig.colorbar(sm)
 cbar.ax.set_ylabel("Percentage of SOW where all criteria are met (%)")
 ax.set_ylabel("Percentage of SOW where each criterion is met (%)", size= 12)
 ax.set_yticks(np.arange(0,110,20))
-ax.set_xticks([0,1,2,3,4])
+ax.set_xticks([0,1,2,3,4,5])
 ax.set_xticklabels(objs_labels)
 ax.legend()
 plt.savefig('robustness_parallel_coordinate.png')
